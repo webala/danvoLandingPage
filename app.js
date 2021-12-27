4131f94040204a99ff78d335e4bbb6a0ba0f439d
webala <webala1001@gmail.com>
2021-12-27 22:07:34 +0300

    hambrger menu complete

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 //select elements
-var toggle_button = document.querySelector('.toggle-btn');
-var big_wrapper = document.querySelector('.big-wrapper');
+var toggle_button;
+var big_wrapper;
+var hamburger_menu;
 const main = document.querySelector('main')
 
 
 let dark = false;
+
+function declare() {
+    toggle_button = document.querySelector('.toggle-btn');
+    big_wrapper = document.querySelector('.big-wrapper');
+    hamburger_menu = document.querySelector('.hamburger-menu')
+}
+function events() {
+    toggle_button.addEventListener('click', toggleAnimation);
+    hamburger_menu.addEventListener('click', () => {
+        big_wrapper.classList.toggle('active');
+    })
+}
+
+
+declare();
+events();
 
 function toggleAnimation() {
 
@@ -19,9 +36,19 @@ function toggleAnimation() {
         clone.classList.remove('dark');
         clone.classList.add('light');
     }
-    clone.classList.add('copy')
 
+    document.body.classList.add('stop-scrolling');
+    clone.classList.add('copy');
     main.appendChild(clone)
+
+    clone.addEventListener("animationend", () => {
+        document.body.classList.remove('stop-scrolling');
+        big_wrapper.remove();
+        clone.classList.remove('copy');
+        //Reset variables
+        declare();
+        events();
+    }); 
 }
 
-toggle_button.addEventListener('click', toggleAnimation);
+
